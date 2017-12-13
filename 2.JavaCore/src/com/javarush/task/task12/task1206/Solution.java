a597388bd66010067b0d5db52ca25753e1d7da3e
Konstantin Dobrenkyi <malefuctor@yandex.ru>
2017-12-13 16:43:03 +0300

    Task 1206 complete

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1206/Solution.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task12.task1206;
+
+/* 
+Выполняем перегрузку!
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(String s) {}
+    public static void print(int i) {}
+}
