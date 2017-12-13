8cd634cfcab590be2eae6ac9ff9cc1efd346306d
Konstantin Dobrenkyi <malefuctor@yandex.ru>
2017-12-13 20:25:50 +0300

    Tasks 1208-1209 complete

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static void print(int i){}
+    public static void print(String i){}
+    public static void print(Boolean i){}
+    public static void print(byte i){}
+    public static void print(double i){}
+
+}
---
+++ 2.JavaCore/src/com/javarush/task/task12/task1209/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1209;
+
+/* 
+Три метода и минимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int min(int i, int j) {
+        if (i>j) return j;
+        else return i;
+    }
+    public static long min(long i, long j) {
+        if (i>j) return j;
+        else return i;
+    }
+    public static double min(double i, double j) {
+        if (i>j) return j;
+        else return i;
+    }
+}
