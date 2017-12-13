17ac8c6307b7f541b1c1de8371212634447a16b2
Konstantin Dobrenkyi <malefuctor@yandex.ru>
2017-12-13 16:48:40 +0300

    Task 1207 complete

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1207/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.task12.task1207;
+
+/* 
+Int и Integer
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    int i = 1;
+    Integer f = 1;
+    print(i);
+    print(f);
+    }
+
+    public static void print(int i){}
+    public static void print(Integer i){}
+}
