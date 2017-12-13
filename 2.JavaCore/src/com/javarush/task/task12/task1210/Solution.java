2f095f3ca0688d515b155b1f19ce943bc89e6f05
Konstantin Dobrenkyi <malefuctor@yandex.ru>
2017-12-13 20:30:24 +0300

    Tasks 1208-1210 complete

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1210/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task12.task1210;
+
+/* 
+Три метода и максимум
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public static int max(int i, int j) {
+        if (i>j) return i;
+        else return j;
+    }
+    public static long max(long i, long j) {
+        if (i>j) return i;
+        else return j;
+    }
+    public static double max(double i, double j) {
+        if (i>j) return i;
+        else return j;
+    }
+}
