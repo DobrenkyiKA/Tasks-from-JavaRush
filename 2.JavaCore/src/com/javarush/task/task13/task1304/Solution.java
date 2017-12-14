e125a55b502f7c7afd9ccd87e630a2caa6bb421e
Konstantin Dobrenkyi <malefuctor@yandex.ru>
2017-12-14 18:53:34 +0300

    Tasks 1301-1310 complete

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1304/Solution.java
@@ -0,0 +1,24 @@
+package com.javarush.task.task13.task1304;
+
+/* 
+Selectable и Updatable
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+    }
+
+    interface Selectable {
+        void onSelect();
+    }
+
+    interface Updatable {
+        void refresh();
+    }
+    public class Screen implements Selectable,Updatable{
+        public void onSelect(){}
+        public void refresh(){}
+    }
+
+    //напишите тут ваш класс
+}
