871a1b91ebbb6208b5553bf8f94243f40997033d
Konstantin Dobrenkyi <malefuctor@yandex.ru>
2017-12-15 23:47:43 +0300

    Tasks 1301-1310 complete

---
+++ 2.JavaCore/src/com/javarush/task/task13/task1315/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task13.task1315;
+
+/* 
+Том, Джерри и Спайк
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+    //может двигаться
+    public interface Movable {
+        void move();
+    }
+
+    //может быть съеден
+    public interface Eatable {
+        void eaten();
+    }
+
+    //может кого-нибудь съесть
+    public interface Eat {
+        void eat();
+    }
+
+    public class Mouse implements Movable,Eatable{
+        public void move(){}
+        public void eaten(){}
+    }
+    public class Cat implements Movable,Eatable,Eat{
+        public void move(){}
+        public void eaten(){}
+        public void eat(){}
+    }
+    public class Dog implements Movable,Eat{
+        public void move(){}
+        public void eat(){}
+    }
+}
