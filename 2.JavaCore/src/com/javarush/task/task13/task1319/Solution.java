6f3d29931b612c0ca06d69ccb7953a2d63a5911d
Konstantin Dobrenkyi <malefuctor@yandex.ru>
2017-12-19 16:35:18 +0300

    Task 1319 complete

--- 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task13/task1319/Solution.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 /* 
 Писатель в файл с консоли
@@ -11,6 +12,20 @@ import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) {
-        // напишите тут ваш код
+
+        try (BufferedReader reader = new BufferedReader (new InputStreamReader(System.in));
+                BufferedWriter writer = new BufferedWriter (new FileWriter(reader.readLine())))
+                {
+                    String text;
+                    do {
+                        text=reader.readLine();
+                        writer.write(text + "\n");
+                        writer.flush();
+                    }
+                        while(!(text.equals("exit")));
+                }
+                catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
     }
 }
