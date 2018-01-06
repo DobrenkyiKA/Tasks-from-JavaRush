781f63fe0a217408703026f248ab8ef2f472efc0
MaleFuctor <MaleFuctor@yandex.ru>
2018-01-06 16:09:53 +0300

    Tasks 1522 - 1525 complete

---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/Solution.java
@@ -0,0 +1,18 @@
+package com.javarush.task.task15.task1523;
+
+/* 
+Перегрузка конструкторов
+*/
+
+public class Solution {
+    Solution() {}
+    public Solution(int x) {}
+    private Solution(double d) {}
+    protected Solution(String s) {}
+
+
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ 2.JavaCore/src/com/javarush/task/task15/task1523/SubSolution.java
@@ -0,0 +1,9 @@
+package com.javarush.task.task15.task1523;
+
+public class SubSolution extends Solution {
+    SubSolution(int i) {}
+    public SubSolution(double d) {}
+    protected SubSolution(String s) {}
+
+
+}
