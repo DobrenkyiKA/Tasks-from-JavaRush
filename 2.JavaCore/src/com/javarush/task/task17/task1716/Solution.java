d7226aa8ce55c674dccc8389af8533f37b05d106
MaleFuctor <MaleFuctor@yandex.ru>
2018-01-15 23:50:53 +0300

    Task 1716 recomplete

--- 2.JavaCore/src/com/javarush/task/task17/task1716/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task17/task1716/Solution.java
@@ -8,41 +8,41 @@ public class Solution {
     private double param = Math.random();
     private StringBuilder sb = new StringBuilder();
 
-    private void method0() {
+    private  void method0() {
         Double d = method3();
     }
 
-    protected void method1(String param1) {
+    protected  void method1(String param1) {
         Solution solution = new Solution();
         solution.method0();
     }
 
-    public void method2(int param1) {
+    public  void method2(int param1) {
         param1++;
     }
 
-    double method3() {
+    synchronized double method3() {
         double random = Math.random();
         param += 40.7;
         return random + param;
     }
 
-    private void method4() {
+    synchronized private void method4() {
         sb.append(1).append(1).append(1).append(1);
     }
 
-    protected void method5(String param2) {
+     protected void method5(String param2) {
         new StringBuffer().append(param2).append(param2).append(param2);
     }
 
-    public String method6(int param2) {
+    synchronized public String method6(int param2) {
         System.out.println("Thinking....");
         method7(5e-2);
         sb = new StringBuilder("Got it!.");
         return sb.toString();
     }
 
-    String method7(double param2) {
+     String method7(double param2) {
         return "" + param2;
     }
 
