fca10acb492cb30b654af1e8b85b1a25e2269471
MaleFuctor <MaleFuctor@yandex.ru>
2018-01-31 04:28:44 +0300

    Task 3212 complete

---
+++ 4.JavaCollections/src/com/javarush/task/task32/task3212/ServiceLocator.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task32.task3212;
+
+import com.javarush.task.task32.task3212.contex.InitialContext;
+import com.javarush.task.task32.task3212.service.Service;
+
+
+public class ServiceLocator {
+    private static Cache cache;
+
+    static {
+        cache = new Cache();
+    }
+
+    public static Service getService(String jndiName){
+
+        Service service = cache.getService(jndiName);
+
+        if(service != null){
+            return service;
+        }
+
+        InitialContext context = new InitialContext();
+        Service service1 = (Service)context.lookup(jndiName);
+        cache.addService(service1);
+        return service1;
+    }
+}
