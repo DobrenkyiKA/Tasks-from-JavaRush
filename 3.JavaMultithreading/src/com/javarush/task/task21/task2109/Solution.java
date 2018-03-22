c12295d6e8f2a8e268b68cea7508cc8bd1859ab0
MaleFuctor <MaleFuctor@yandex.ru>
2018-03-22 22:12:28 +0300

    Task 2109 complete

---
+++ 3.JavaMultithreading/src/com/javarush/task/task21/task2109/Solution.java
@@ -0,0 +1,68 @@
+package com.javarush.task.task21.task2109;
+
+/* 
+Запретить клонирование
+*/
+public class Solution {
+    public static class A implements Cloneable {
+        private int i;
+        private int j;
+
+        public A(int i, int j) {
+            this.i = i;
+            this.j = j;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public int getJ() {
+            return j;
+        }
+    }
+
+    public static class B extends A {
+        private String name;
+
+        public B(int i, int j, String name) {
+            super(i, j);
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        protected B clone() throws CloneNotSupportedException {
+                throw new CloneNotSupportedException();
+        }
+    }
+
+    public static class C extends B {
+        public C(int i, int j, String name) {
+            super(i, j, name);
+        }
+
+        @Override
+        protected C clone()  {
+            C c = new C(this.getI(),this.getJ(),this.getName());
+            return c;
+        }
+
+        @Override
+        public int hashCode() {
+            return super.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            return super.equals(obj);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
