60bb247a14720c36835a9c9e595df52f5b322fe1
MaleFuctor <MaleFuctor@yandex.ru>
2018-03-31 14:15:44 +0300

    Task 2203 complete

---
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2203/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.task22.task2203;
+
+/* 
+Между табуляциями
+*/
+public class Solution {
+    public static String getPartOfString(String string) throws TooShortStringException {
+        if (string == null || string.isEmpty())
+            throw new TooShortStringException();
+
+            String[] str = string.split("\t");
+        if (str.length < 3) throw new TooShortStringException();
+        return str[1];
+    }
+
+    public static class TooShortStringException extends Exception {
+    }
+
+    public static void main(String[] args) throws TooShortStringException {
+        System.out.println(getPartOfString("\tJavaRush - лучший сервис \tобучения Java\t."));
+    }
+}
