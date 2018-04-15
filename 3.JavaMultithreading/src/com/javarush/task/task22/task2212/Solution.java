2ffc2b8f5a3431bfc62bb53555ae9c2c4031ad9e
MaleFuctor <MaleFuctor@yandex.ru>
2018-04-15 23:33:35 +0300

    Task 2212 complete

--- 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task22/task2212/Solution.java
@@ -1,14 +1,18 @@
 package com.javarush.task.task22.task2212;
-
-/* 
+/*
 Проверка номера телефона
 */
 public class Solution {
     public static boolean checkTelNumber(String telNumber) {
-        return false;
+        if (telNumber == null || telNumber == "") return false;
+        else {
+            int i = telNumber.replaceAll("\\D", "").length();
+            if ((telNumber.charAt(0) == '+' && i == 12) || (telNumber.charAt(0) != '+' && i == 10)) {
+                return telNumber.matches("\\+?\\d+(\\(\\d{3}\\))?\\d+-?\\d+-?\\d+");
+            } else return false;
+        }
     }
 
     public static void main(String[] args) {
-
     }
 }
